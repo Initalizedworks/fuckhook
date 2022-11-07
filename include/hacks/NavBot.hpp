04531ff214b1cbc1f902de7dc33208cf6e6e2b3f
joe20050108 <buckybabaian@gmail.com>
2022-11-07 18:56:17 +0000

    Added explowz prescope logic.

--- include/hacks/NavBot.hpp
+++ include/hacks/NavBot.hpp
@@ -1,55 +1,7 @@
 #pragma once
 
-#include <array>
-#include <stdint.h>
-
 namespace hacks::NavBot
 {
-bool init(bool first_cm);
-namespace task
-{
-
-enum task : uint8_t
-{
-    none = 0,
-    sniper_spot,
-    stay_near,
-    health,
-    ammo,
-    dispenser,
-    followbot,
-    outofbounds,
-    capture
-};
-
-struct Task
-{
-    task id;
-    int priority;
-    Task(task id)
-    {
-        this->id = id;
-        priority = id == none ? 0 : 5;
-    }
-    Task(task id, int priority)
-    {
-        this->id       = id;
-        this->priority = priority;
-    }
-    operator task()
-    {
-        return id;
-    }
-};
-
-constexpr std::array<task, 3> blocking_tasks{ followbot, outofbounds };
-extern Task current_task;
-} // namespace task
-struct bot_class_config
-{
-    float min;
-    float preferred;
-    float max;
-};
+std::pair<CachedEntity *, float> getNearestPlayerDistance();
 } // namespace hacks::NavBot
 
--- src/hacks/NavBot.cpp
+++ src/hacks/NavBot.cpp
@@ -13,6 +13,7 @@
 #include "navparser.hpp"
 #include "MiscAimbot.hpp"
 #include "Misc.hpp"
+#include "NavBot.hpp"
 
 namespace hacks::NavBot
 {
@@ -22,8 +23,6 @@ static settings::Boolean search_ammo("navbot.search-ammo", "false");
 static settings::Boolean stay_near("navbot.stay-near", "false");
 static settings::Boolean capture_objectives("navbot.capture-objectives", "false");
 static settings::Boolean defend_payload("navbot.defend-payload", "false");
-static settings::Boolean autozoom("navbot.autozoom.enabled", "false");
-static settings::Float zoom_distance("navbot.autozoom.trigger-distance", "600");
 static settings::Int melee_range("navbot.primary-only-melee-range", "150");
 static settings::Boolean snipe_sentries("navbot.snipe-sentries", "false");
 static settings::Boolean escape_danger("navbot.escape-danger", "false");
@@ -260,7 +259,7 @@ void refreshSniperSpots()
                 sniper_spots.emplace_back(hiding_spot.m_pos);
 }
 
-static std::pair<CachedEntity *, float> getNearestPlayerDistance()
+std::pair<CachedEntity *, float> getNearestPlayerDistance()
 {
     float distance         = FLT_MAX;
     CachedEntity *best_ent = nullptr;
@@ -1336,29 +1335,6 @@ bool escapeDanger()
 
 static int slot = primary;
 
-static void autoZoom(std::pair<CachedEntity *, float> &nearest)
-{
-    if (!autozoom)
-        return;
-    if (CE_BAD(LOCAL_E) || !LOCAL_E->m_bAlivePlayer() || CE_BAD(LOCAL_W))
-        return;
-    if (g_pLocalPlayer->holding_sniper_rifle)
-        return;
-    // Zoom in and update timeinzoom
-    if (g_pLocalPlayer->holding_sniper_rifle && nearest.second <= *zoom_distance && !g_pLocalPlayer->bZoomed)
-        current_user_cmd->buttons |= IN_ATTACK2;
-    if (g_pLocalPlayer->bZoomed && g_pLocalPlayer->holding_sniper_rifle &&!nearest.second <= *zoom_distance)
-        current_user_cmd->buttons |= IN_ATTACK2;
-    if (LOCAL_W->m_iClassID() == CL_CLASS(CTFMinigun) && nearest.second <= *zoom_distance)
-        current_user_cmd->buttons |= IN_JUMP | IN_ATTACK2;
-}
-
-static void autoZoomEnabled(std::pair<CachedEntity *, float> &nearest)
-{
-    if (!nearest.second <= *zoom_distance)
-        return;
-}
-
 static slots getBestSlot(slots active_slot, std::pair<CachedEntity *, float> &nearest)
 {
     if (force_slot)
@@ -1514,7 +1490,6 @@ void CreateMove()
     auto nearest = getNearestPlayerDistance();
 
     updateSlot(nearest);
-    autoZoom(nearest);
     updateEnemyBlacklist(slot);
 
     if (meleeAttack(slot, nearest))
